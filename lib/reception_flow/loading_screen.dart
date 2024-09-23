94dc5ee06a3a07731dc67e6024321415ab9af63e
m0dern6 <na.ab.en.153@gmail.com>
2024-09-23 13:15:13 +0545

    loading screen done

--- lib/reception_flow/loading_screen.dart
+++ lib/reception_flow/loading_screen.dart
@@ -1,10 +1,44 @@
 import 'package:flutter/material.dart';
 
-class LoadingScreen extends StatelessWidget {
+class LoadingScreen extends StatefulWidget {
   const LoadingScreen({super.key});
 
   @override
+  State<LoadingScreen> createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 2), () {
+      Navigator.pushReplacementNamed(context, 'sign_in');
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      color: Colors.white,
+      width: double.infinity,
+      height: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Image.asset('assets/logo.png'),
+          const SizedBox(
+            height: 40,
+          ),
+          const Padding(
+            padding: EdgeInsets.only(left: 10, right: 10),
+            child: LinearProgressIndicator(
+              minHeight: 4,
+              valueColor: AlwaysStoppedAnimation<Color>(Colors.green),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
